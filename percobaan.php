4ddc08c98d4cb056f033f17a511c07e3f356d7ce
ValentinoRaflii <asinua520@gmail.com>
2025-10-21 11:33:15 +0800

    Create percobaan.php

---
+++ percobaan.php
@@ -0,0 +1,21 @@
+<div class="col-lg-4 col-md-6 portfolio-item tinanggea">
+  <div class="project-card">
+    <div class="project-image">
+      <a href="assets/img_T/portfolio/01-small.jpg" 
+         title="KELURAHAN TINANGGEA - PESISIR PANTAI"
+         data-lightbox-gallery="gallery-tinanggea" 
+         class="nivo-lightbox">
+        <img src="assets/img_T/portfolio/01-small.jpg" 
+             alt="Project Title" 
+             class="img-fluid">
+        <div class="project-overlay">
+          <div class="project-actions">
+            <h2 style="color: #fff; text-shadow: 0 0 10px rgba(0,0,0,0.8);">
+              PESISIR PANTAI
+            </h2>
+          </div>
+        </div>
+      </a>
+    </div>
+  </div>
+</div>
